f487638bb80954a6934a368d8da4c2bb72a8d265
jordisanchezcarbonell <47449712+jordisanchezcarbonell@users.noreply.github.com>
2019-12-20 12:28:04 +0100

    Cambioerrores

--- js/app.js
+++ js/app.js
@@ -94,7 +94,7 @@ $(document).ready(function () {
 
   // Evento para whastapp
   $('#wpp').on('click', function(){
-    window.open("https://api.whatsapp.com/send?phone=51971313111&text=I'm%20interested%20in%20your%20portfolio");
+    window.open("https://api.whatsapp.com/send?phone=34676564769&text=Estoy%20interesado%20in%20en%20tu%20portfolio");
   })
 
   // modal
