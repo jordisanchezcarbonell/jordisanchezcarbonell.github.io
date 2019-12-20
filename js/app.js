53653c9142eb80dae10515216685fd186505a46e
jordisanchezcarbonell <47449712+jordisanchezcarbonell@users.noreply.github.com>
2019-12-20 14:14:58 +0100

    Update

--- js/app.js
+++ js/app.js
@@ -65,7 +65,7 @@
 
   // $projectImg.on('click', showInfo);
   function apiWhats(){
-    window.open("https://api.whatsapp.com/send?phone=51971313111&text=I'm%20interested%20in%20your%20portfolio");
+    window.open("https://api.whatsapp.com/send?phone=34676564769&text=Hola%20me%20interesa%20tu%20perfil");
   
   
   }
